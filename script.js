cc6799fa0cc826cd678c141f56a9fbc7dd065f37
Albert Laypanov <alaypanov.exceedteam@gmail.com>
2019-08-06 18:54:34 +0300

    add class

--- script.js
+++ script.js
@@ -1,96 +1,41 @@
-let student = {
-    name: 'Jhon',
-    age: 30,
-    isAdmin: false,
-    courses: ['html', 'css', 'js'],
-    wife: null
-}
+$(document).ready( () => {
+    const todo = new ToDo()
 
-let json = JSON.stringify(student)
-console.log(typeof json)
-console.log('jquery tag', $('p'))
-$('#idd').on('click',  () => {
-    console.log($('#idd').attr('class'))
-    let x = $('#idd').attr('class').replace('a', 'hello moto ')
-    console.log(x)
+    $('#add-button').on('click', () => todo.addItem())
+
+
 })
 
+class ToDo {
 
-console.log('jquery class', $('.asd'))
-console.log('jquery id', $('#idd'))
+    addItem () {
+        let text = 'asdasd'
+        let color = this.getColorByCkassName('blue')
+        $('.task-container').append(
+            `
+                <div class="item ${color}">
+                    <div class="checkbox">
+                        <input type="checkbox" class="form-check-input">
+                    </div>
+                    <div class="task">Task #1   ${text}</div>
+                </div>
+            `
+        )
+    }
 
-// let say = setTimeout(function tick() {
-//     console.log('tack')
-//     say = setTimeout(tick, 2000)
-// }) 
-// рекруссивный setTimeout
+    getColorByCkassName() {
+        let className = $('.ac')
+        switch (className) {
+            case 'red':
+                return 'red'
+                break;
+        
 
-// function say(arg, arg1) {
-//     console.log(arg + ' ' + arg1)
-// }
-// setInterval(say, 2000, "hello", "world")
-
-// function say (arg, arg1) {
-//     console.log(arg + ' ' + arg1)
-// }
-// setTimeout(say, 3000, "Hello", "world")
-
-// let timerId = setInterval(() => console.log('tick'), 2000)
-
-// setInterval(() => {
-//     clearInterval(timerId)
-//     console.log('stop')
-// }, 5000)
-
-
-
-
-// let array = ["Apple", "Orange", "Banana"]
-// array.push("Orange")
-// array.unshift("Banana")
-// array.push({a: 1})
-// array.push("555")
-// array.pop("555")
-// array.splice(3, 1)
-// console.log(array)
-
-// console.log(array.shift())
-// console.log(array)
-// console.log(array.length)
-// console.log(
-//     array.filter((elem, index) => {
-//         console.log(elem, index)
-//         typeOf(elem) == 'object'
-//     })
-// )
-
-// function a () {
-//     var x
-//     if (a==a){
-
-//     }
-// }
-
-
-
-// if (x.indexOf('my') != -1) {
-//     alert('YES')
-// }
-
-// let start = x.slice(0, 2)
-// let end = x.slice(3)
-
-// let changeSymbol = 'u'
-
-
-// // console.log( start + changeSymbol + end )
-
-
-// function changeThirdSymbol (word, number, changeSymbol) {
-//     let start = word.slice(0, number)
-//     let end = word.slice(number+1)
-
-//     return start + changeSymbol + end
-// }
-
-// console.log (changeThirdSymbol(x, 40, 'p') ) 
+            case 'blue':
+                return 'blue'
+                break;
+            default:
+                break;
+        }
+    }
+}
